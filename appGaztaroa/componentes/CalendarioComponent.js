6d80a55852ab02055b3af5a3c93b77e506b312f3
Portatil Eve <evevanetene@gmail.com>
2024-04-19 13:18:01 +0200

    Stack Navigation

--- appGaztaroa/componentes/CalendarioComponent.js
+++ appGaztaroa/componentes/CalendarioComponent.js
@@ -1,37 +1,45 @@
-import React from 'react';
-import { ListItem, Avatar, Text } from '@rneui/themed'; // Asegúrate de importar el componente Text
+import React, { Component } from 'react';
+import { ListItem, Avatar } from '@rneui/themed';
 import { SafeAreaView, FlatList } from 'react-native';
+import { EXCURSIONES } from '../comun/excursiones';
 
-function Calendario(props) {
-    const renderCalendarioItem = ({ item, index }) => {
+class Calendario extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            excursiones: EXCURSIONES
+        };
+    }
+
+    render(){
+
+    const { navigate } = this.props.navigation;    
+
+    const renderCalendarioItem = ({item, index}) => {
         return (
             <ListItem
-                key={index}
-                onPress={() => props.onPress(item.id)}
-                bottomDivider
-            >
+            key={index}
+            onPress={() => navigate('DetalleExcursion', { excursionId: item.id })}
+            bottomDivider>
                 <Avatar source={require('./imagenes/40Años.png')} />
                 <ListItem.Content>
-                    <ListItem.Title>
-                        <Text>{item.nombre}</Text>
-                    </ListItem.Title>
-                    <ListItem.Subtitle>
-                        <Text>{item.descripcion}</Text>
-                    </ListItem.Subtitle>
+                    <ListItem.Title>{item.nombre}</ListItem.Title>
+                    <ListItem.Subtitle>{item.descripcion}</ListItem.Subtitle>
                 </ListItem.Content>
-            </ListItem>
+            </ListItem> 
         );
     };
 
     return (
         <SafeAreaView>
-            <FlatList
-                data={props.excursiones}
+            <FlatList 
+                data={this.state.excursiones}
                 renderItem={renderCalendarioItem}
                 keyExtractor={item => item.id.toString()}
             />
         </SafeAreaView>
     );
+    }
 }
 
 export default Calendario;
